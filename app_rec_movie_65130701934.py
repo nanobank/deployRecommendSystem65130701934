7f9c62a0fc831eed3bbde5758d80fe53bbfe60a3
nanobank <56763392+nanobank@users.noreply.github.com>
2024-11-03 14:48:57 +0700

    Add files via upload

---
+++ app_rec_movie_65130701934.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pickle
+
+# Define a function to load the data from GitHub
+@st.cache_data  # Cache the data to avoid re-downloading
+def load_data():
+    with open('./recommendation_movie_svd.pkl', 'rb' ) as file:
+        svd_model,movie_ratings,movies = pickle.load(file)
+    return svd_model, movie_ratings, movies
+
+# Load the data
+svd_model, movie_ratings, movies = load_data()
+# Streamlit app title
+st.title("Movie Recommender")
+# Check if data is successfully loaded
